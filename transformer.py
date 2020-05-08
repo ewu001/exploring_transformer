116610c0471f2122b7d800e0b29f5692d62407ba
ewu001 <59420306+ewu001@users.noreply.github.com>
2020-05-07 21:49:39 -0400

    Rename

--- transformer.py
+++ transformer.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-class EncoderDecoder(nn.Module):
+class Transformer(nn.Module):
     '''
     Provides abstraction for encoder to decoder architecture,
     Encoder takes input (batch, sentence_length), projects to embedding space then outputs 
@@ -11,7 +11,7 @@ class EncoderDecoder(nn.Module):
     Generator includes a linear projection of decoder output and map to softmax regression for output probability distribution
     '''
     def __init__(self, encoder, decoder, generator, src_embed, tgt_embed):
-        super(EncoderDecoder, self).__init__()
+        super(Transformer, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
         self.generator = generator
@@ -19,14 +19,7 @@ class EncoderDecoder(nn.Module):
         self.tgt_embed = tgt_embed
 
     def forward(self, src, src_mask, tgt, tgt_mask):
-        '''
-        
-        '''
+
         pass
 
-    def encode(self, src, src_mask):
-        pass
-
-    def decode(self, tgt, tgt_mask):
-        pass
 
