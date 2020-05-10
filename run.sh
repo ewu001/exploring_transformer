1b18c36ba03c2617a5d1d7ffcfd0582d88c4dae6
ewu001 <59420306+ewu001@users.noreply.github.com>
2020-05-10 13:50:20 -0400

    update greedy decoding

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ elif [ "$1" = "test" ]; then
 elif [ "$1" = "train_local" ]; then
 	python run.py train --train-src=./en_es_data/train.en --train-tgt=./en_es_data/train.es --dev-src=./en_es_data/dev.en --dev-tgt=./en_es_data/dev.es --vocab=vocab.json
 elif [ "$1" = "test_local" ]; then
-    python run.py decode model.bin ./en_es_data/test.en ./en_es_data/test.es outputs/test_outputs.txt
+    python run.py decode model.bin ./en_es_data/dev.en ./en_es_data/dev.es outputs/test_outputs.txt
 elif [ "$1" = "vocab" ]; then
 	python vocab.py --train-src=./en_es_data/train.en --train-tgt=./en_es_data/train.es vocab.json
 else
